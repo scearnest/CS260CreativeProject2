57afbd3cd90ea9110964f96c1ba43d04aa6638f2
Samuel Earnest <earnestcsamuel@gmail.com>
2019-05-15 15:41:58 -0600

    js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 window.onload = function() {
-  document.getElementById("submit").addEventListener("click", async function(event) {
+  document.getElementById("search").addEventListener("click", async function(event) {
     event.preventDefault();
     // const value = document.getElementById("weatherInput").value;
     // if (value === "")
     // return;
     // console.log(value);
 
-    const url = "https://api.edamam.com/search?q=salt,oil&app_id=f4381c19&app_key=8e9220e0d74298ab22143e7237ab941f";
+
+    ingredients = document.getElementById("searchbar").value;
+
+    health = getHealthString();
+    // calories = getCalories();
+
+
+
+    const url = "https://api.edamam.com/search?q=" + ingredients + "&app_id=f4381c19&app_key=8e9220e0d74298ab22143e7237ab941f" + health;
+
+    console.log(url);
+
     // "https://api.edamam.com/search? " + value + ",US&units=imperial" +
     // "&APPID=2992349419d0a9f42eb9de5f88bad8b4";
     try {
@@ -20,3 +31,39 @@ window.onload = function() {
     }
   });
 }
+
+
+function getHealthString() {
+  health = "";
+
+  if(document.getElementById("alcohol").checked)
+  {
+    health += "&health=alcohol-free	";
+  }
+  if(document.getElementById("lactose").checked)
+  {
+    health += "&health=dairy-free";
+  }
+  if(document.getElementById("gluten").checked)
+  {
+    health += "&health=gluten-free";
+  }
+  if(document.getElementById("kosher").checked)
+  {
+    health += "&health=kosher";
+  }
+  if(document.getElementById("vegan").checked)
+  {
+    health += "&health=vegan";
+  }
+  if(document.getElementById("vegetarian").checked)
+  {
+    health += "&health=vegetarian";
+  }
+
+  return health;
+}
+
+// function getCalories() {
+//   calories = "";
+// }
